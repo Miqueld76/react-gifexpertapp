c4ae5b6907e734271854d98a1b7499fe729059a1
Miqueld76 <mikid76@gmail.com>
2022-04-30 10:30:13 +0200

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=N1xhTpS6XuWKp4br8A2L5E58IbH11G8z`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        }
+    })
+
+    return gifs;
+}
